997af1ff2ad3bff9a5ce85ff6fbbee730e64f04c
chajus1 <chajus1@googlemail.com>
2023-07-06 01:37:34 +0100

    simplify

--- Cards8SuitsAndValues/CardsComparerByValue.cs
+++ Cards8SuitsAndValues/CardsComparerByValue.cs
@@ -6,14 +6,8 @@ namespace Cards8SuitsAndValues
     {
         public int Compare(Card x, Card y)
         {
-            if (x.Suit < y.Suit)
-                return -1;
-            if (x.Suit < y.Suit)
-                return 1;
-            if (x.Value < y.Value)
-                return -1;
-            if (x.Value > y.Value)
-                return 1;
+            if (x.Suit < y.Suit || x.Value < y.Value) return -1;
+            if (x.Suit > y.Suit || x.Value > y.Value) return 1;
             return 0;
         }
     }
